230d109887762a112e1eb64d4c84b4cd6252ce5d
camiloerazo <juancamiloerazo82@gmail.com>
2018-04-24 15:47:59 -0500

    Implementacion de la multiplicacion

--- Taller_herramientas.py
+++ Taller_herramientas.py
@@ -1,13 +1,14 @@
 def calculadora():
-    operacion = str(input("Digite operacion"))
+    operacion = str(input("Digite operacion: "))
     n1 = int(input("Digite primer numero:"))
     n2 = int(input("Digite segundo numero:"))
     if operacion == '+' :
         resultado = n1 + n2
-        return(resultado)
+        return resultado
     if operacion == '-' :
         resultado = n1 - n2
-        return(resultado)
-
-
+        return resultado
+    if operacion == "*" :
+        resultado = n1*n2
+        return resultado
 print(calculadora())
