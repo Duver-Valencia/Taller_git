c0fec6087777771afdf3da22cdc41f5d028e6581
Duver Valencia <duvervalencia02@gmail.com>
2018-04-24 13:48:58 -0500

    Implementada la suma

---
+++ Taler_herramientas.py
@@ -0,0 +1,9 @@
+def calculadora():
+    operacion = str(input("Digite operacion"))
+    n1 = int(input("Digite primer numero:"))
+    n2 = int(input("Digite segundo numero:"))
+    if operacion == '+' :
+        resultado = n1 + n2
+        return(resultado)
+
+print(calculadora())
